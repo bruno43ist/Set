c74a444cadef851b301ab0f97a0342205407203f
Bruno Farias <bruno43ist@yahoo.com.br>
2019-08-13 19:57:28 -0300

    Operações de conjuntos

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,35 +1,31 @@
 package application;
 
+import java.util.Arrays;
 import java.util.LinkedHashSet;
 import java.util.Set;
+import java.util.TreeSet;
 
 public class Program {
 
 	public static void main(String[] args) {
 		
-		//HashSet não mantem a ordem dos elementos
-		//Set<String> set = new HashSet<>();
+		Set<Integer> a = new TreeSet<Integer>(Arrays.asList(0,2,4,5,6,7,10));
+		Set<Integer> b = new TreeSet<Integer>(Arrays.asList(5,6,7,8,9,10));
 		
-		//TreeSet ordena os elementos
-		//Set<String> set = new TreeSet<>(); 
+		//union
+		Set<Integer> c = new TreeSet<>(a);
+		c.addAll(b);
+		System.out.println(c);
 		
-		//LinkedHashSet mantem a ordem dos elementos
-		Set<String> set = new LinkedHashSet<>();
+		//intersection
+		Set<Integer> d = new TreeSet<Integer>(a);
+		d.retainAll(b);
+		System.out.println(d);
 		
-		
-		//HASHSET NÃO GARANTE A ORDEM DOS ELEMENTOS
-		
-		set.add("TV");
-		set.add("Tablet");
-		set.add("Notebook");
-		
-		
-		//set.removeIf(x -> x.length() >= 3);//REMOVE TODO MUNDO QUE TEM MAIS DE 3 CARACTERES
-		set.removeIf(x -> x.charAt(0) == 'T');//REMOVE TODO MUNDO QUE COMEÇA COM A LETRA T
-		
-		for(String p : set) {
-			System.out.println(p);
-		}
+		//difference
+		Set<Integer> e = new TreeSet<Integer>(a);
+		e.removeAll(b);
+		System.out.println(e);
 
 	}
 
