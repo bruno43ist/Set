179bbac1bcd0e40c66b7e5563dba28908d69cde3
Bruno Farias <bruno43ist@yahoo.com.br>
2019-08-13 20:14:28 -0300

    comparações de treeSets

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,7 +1,7 @@
 package application;
 
-import java.util.HashSet;
 import java.util.Set;
+import java.util.TreeSet;
 
 import entities.Product;
 
@@ -9,16 +9,15 @@ public class Program {
 
 	public static void main(String[] args) {
 		
-		Set<Product> set = new HashSet<>();
+		Set<Product> set = new TreeSet<>();
 		
-		set.add(new Product("TV", 900.0));
+		set.add(new Product("Tv", 900.0));
 		set.add(new Product("Notebook", 1200.0));
 		set.add(new Product("Tablet", 400.0));
 		
-		Product prod = new Product("Notebook", 1200.0);
-		
-		System.out.println(set.contains(prod));//testa se o produto prod está contido no set(conjunto)
+		for(Product p : set) {
+			System.out.println(p);
+		}
 
 	}
-
 }
